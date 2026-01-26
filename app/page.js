e0ca8697d6c088b16cd67c19357c1ecae141a9aa
JIYOON <psyoonchild@gmail.com>
2026-01-26 12:51:43 +0900

    UI 텍스트 수정

--- app/page.js
+++ app/page.js
@@ -1068,7 +1068,7 @@ export default function OnlineLibraryDashboard() {
             type="text"
             value={newMessage}
             onChange={(e) => setNewMessage(e.target.value)}
-            placeholder="응원 메시지를 입력하세요..."
+            placeholder="메시지를 입력하세요!"
             className="flex-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             maxLength={200}
           />
